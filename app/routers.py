6d5225fd9b8b98b069f5de30e9adcabc3ba904d9
Festus Omaku <fomaku3@gmail.com>
2023-06-26 15:47:54 +0100

    update

--- app/routers.py
+++ app/routers.py
@@ -1,24 +1,41 @@
 from flask import Flask, request, jsonify
+import librosa
+import numpy as np
+import tensorflow as tf
 
 app = Flask(__name__)
 
-@app.route('/classify', methods=['POST'])
-def classify_mood():
-    # Handles the incoming audio data
-    audio_data = request.files['audio']
-    
-    # Performs mood classification using your existing code or libraries
-    predicted_mood = classify_emotion(audio_data)
-    
-    recommended_songs = get_song_recommendations(predicted_mood)
-    
-    # Prepare the response
-    response = {
-            'mood': predicted_mood,
-            'songs': recommended_songs
-            }
-    
-    return jsonify(response)
+# Load the pre-trained emotion detection model
+model = tf.keras.models.load_model('model.h5')
+
+@app.route('/audio', methods=['POST'])
+def save_audio():
+    audio_file = request.files['audio']
+    audio_path = 'recorded_audio.wav'
+    audio_file.save(audio_path)
+    return jsonify({'message': 'Audio file saved successfully.'})
+
+@app.route('/detect-emotion', methods=['POST'])
+def detect_emotion():
+    audio_path = 'recorded_audio.wav'
+    predicted_emotion = classify_emotion(audio_path)
+    return jsonify({'emotion': predicted_emotion})
+
+def classify_emotion(audio_path):
+    audio, sr = librosa.load(audio_path, duration=3)  # Load audio file (adjust duration if needed)
+    features = extract_features(audio)  # Extract audio features
+    features = np.expand_dims(features, axis=0)  # Add batch dimension
+    prediction = model.predict(features)[0]  # Make prediction
+    emotion_moods = ['Neutral', 'Calm', 'Happy', 'Sad', 'Angry', 'Fearful', 'Disgust', 'Surprised']
+    predicted_emotion = emotion_moods[np.argmax(prediction)]
+    return predicted_emotion
+
+def extract_features(audio):
+    chroma_stft = librosa.feature.chroma_stft(y=audio, sr=sr)
+    mfcc = librosa.feature.mfcc(y=audio, sr=sr)
+    features = np.concatenate((chroma_stft.mean(axis=1), mfcc.mean(axis=1)))  # Combine features
+    return features
 
 if __name__ == '__main__':
-    app.run(0.0.0.0, 5000)
+    app.run()
+
--- web_page/emoplay.js
+++ web_page/emoplay.js
@@ -6,6 +6,49 @@ document.addEventListener("DOMContentLoaded", function() {
 	const prevButton = document.getElementById("prevButton");
 	const nextButton = document.getElementById("nextButton");
 
+	let audioChunks = [];
+	
+	let mediaRecorder;
+	navigator.mediaDevices.getUserMedia({ audio: true })
+		.then(function(stream) {
+			mediaRecorder = new MediaRecorder(stream);
+			
+			recordButton.addEventListener("click", function() {
+				if (mediaRecorder.state === "inactive") {
+					audioChunks = [];
+					mediaRecorder.start();
+				} else {
+					mediaRecorder.stop();
+				}
+			});
+			
+			mediaRecorder.addEventListener("dataavailable", function(event) {
+				audioChunks.push(event.data);
+			});
+			
+			mediaRecorder.addEventListener("stop", function() {
+				const audioBlob = new Blob(audioChunks, { type: "audio/wav" });
+				sendAudioToServer(audioBlob);
+			});
+		})
+		.catch(function(error) {
+			console.error("Error accessing audio media:", error);
+		});
+	
+	function sendAudioToServer(audioBlob) {
+		const formData = new FormData();
+		formData.append("audio", audioBlob);
+		
+		fetch("/detect-emotion", { method: "POST", body: formData })
+			.then(response => response.json())
+			.then(data => {
+				console.log("Emotion detected:", data);
+			})
+			.catch(error => {
+				console.error("Emotion detection error:", error);
+			});
+	}
+
 	recordButton.addEventListener("click", function() {
 		// TODO: Handle audio recording logic
 		console.log("Recording audio...");

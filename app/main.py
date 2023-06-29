57978f91c5c97bd858d714d8bf63b2c72d6f6e7d
Omaku Festus Friday <fomaku3@gmail.com>
2023-06-29 22:22:08 +0100

    new method

--- app/main.py
+++ app/main.py
@@ -1,35 +1,44 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+# Spotify API credentials
+CLIENT_ID = '6b39c5ac11d84059b461372141789b59'
+CLIENT_SECRET = '29ffe2e6aaca4bda8738bf60faaaf8c3'
+REDIRECT_URI = 'https://http://127.0.0.1:5000/Emoplay'
+
+# Spotify API endpoints
+AUTH_URL = 'https://accounts.spotify.com/authorize'
+TOKEN_URL = 'https://accounts.spotify.com/api/token'
+API_BASE_URL = 'https://api.spotify.com/v1'
+
+# Flask route to render the HTML template
 @app.route('/')
 def index():
     return render_template('emoplay.html')
 
-@app.route('/get-playlist', methods=['POST'])
-def get_playlist():
-    data = request.get_json()
-    emotion = data.get('emotion')
+# Flask route to handle the Spotify authorization flow
+@app.route('/spotify_callback')
+def spotify_callback():
+    # Authorization code received from Spotify
+    auth_code = request.args.get('code')
 
-    # Retrieve the playlist based on the selected emotion
-    playlist = get_playlist_by_emotion(emotion)
+    # Exchange the authorization code for an access token
+    data = {
+        'grant_type': 'authorization_code',
+        'code': auth_code,
+        'redirect_uri': REDIRECT_URI,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET
+    }
+    response = requests.post(TOKEN_URL, data=data)
 
-    # You can customize the playlist response based on your requirements
-    return jsonify({'playlist': playlist})
+    # Extract the access token from the response
+    access_token = response.json().get('access_token')
 
-def get_playlist_by_emotion(emotion):
-    # Add your logic here to determine the playlist based on the emotion
-    # You can use conditionals or any other method to select the appropriate playlist
-    if emotion == 'sad':
-        return ['Song 1', 'Song 2', 'Song 3']
-    elif emotion == 'happy':
-        return ['Song 4', 'Song 5', 'Song 6']
-    elif emotion == 'excited':
-        return ['Song 7', 'Song 8', 'Song 9']
-    elif emotion == 'indifferent':
-        return ['Song 10', 'Song 11', 'Song 12']
-    else:
-        return []
+    # Redirect to the main page with the access token as a URL parameter
+    return redirect(url_for('index', access_token=access_token))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- app/static/emoplay.js
+++ app/static/emoplay.js
@@ -1,67 +1,93 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const recordButton = document.getElementById("recordButton");
+  const sadButton = document.getElementById("sadButton");
+  const happyButton = document.getElementById("happyButton");
+  const excitedButton = document.getElementById("excitedButton");
+  const indifferentButton = document.getElementById("indifferentButton");
   const playButton = document.getElementById("playButton");
   const prevButton = document.getElementById("prevButton");
   const nextButton = document.getElementById("nextButton");
-  const shuffleButton = document.getElementById("shuffleButton");
-  const recordAnimation = document.getElementById("recordAnimation");
-  const emotionButtons = document.querySelectorAll(".emotion-buttons button");
 
-  let audioChunks = [];
+  let currentPlaylistId = "";
 
-  // Add event listeners to the emotion buttons
-  emotionButtons.forEach(function(button) {
-    button.addEventListener("click", function() {
-      const selectedEmotion = button.dataset.emotion;
-      getPlaylist(selectedEmotion);
+  // Initialize Spotify Web Playback SDK
+  window.onSpotifyWebPlaybackSDKReady = () => {
+    const player = new Spotify.Player({
+      name: 'Web Playback SDK Quick Start Player',
+      getOAuthToken: cb => { cb('<YOUR_SPOTIFY_ACCESS_TOKEN>'); },
+      volume: 0.5
     });
-  });
 
-  // Function to get the playlist based on the selected emotion
-  function getPlaylist(emotion) {
-    fetch("/get-playlist", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ emotion: emotion }),
-    })
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        // Handle the received playlist data
-        const playlist = data.playlist;
-        console.log("Received playlist:", playlist);
-      })
-      .catch(function(error) {
-        console.error("Error retrieving playlist:", error);
+    // Connect to the Spotify player
+    player.connect().then(success => {
+      if (success) {
+        console.log('The Web Playback SDK successfully connected to Spotify!');
+      }
+    }).catch(error => {
+      console.error('Failed to connect to Spotify:', error);
+    });
+
+    // Toggle play/pause button
+    playButton.addEventListener("click", () => {
+      player.togglePlay().then(() => {
+        console.log('Toggle playback');
+      }).catch(error => {
+        console.error('Failed to toggle playback:', error);
       });
-  }
+    });
 
-  // Add event listeners to the control buttons
-  recordButton.addEventListener("click", function() {
-    // TODO: Handle audio recording logic
-    console.log("Recording audio...");
-  });
+    // Play the next track in the playlist
+    nextButton.addEventListener("click", () => {
+      player.nextTrack().then(() => {
+        console.log('Next track');
+      }).catch(error => {
+        console.error('Failed to play next track:', error);
+      });
+    });
 
-  playButton.addEventListener("click", function() {
-    // TODO: Handle music playback logic
-    console.log("Playing music...");
-  });
+    // Play the previous track in the playlist
+    prevButton.addEventListener("click", () => {
+      player.previousTrack().then(() => {
+        console.log('Previous track');
+      }).catch(error => {
+        console.error('Failed to play previous track:', error);
+      });
+    });
 
-  prevButton.addEventListener("click", function() {
-    // TODO: Handle previous song logic
-    console.log("Playing previous song...");
-  });
+    // Play the selected playlist based on the emotion
+    const playPlaylist = (playlistId) => {
+      if (currentPlaylistId !== playlistId) {
+        currentPlaylistId = playlistId;
+        player.pause().then(() => {
+          player.setVolume(0.5).then(() => {
+            player.resume().then(() => {
+              player.play({
+                context_uri: `spotify:playlist:${playlistId}`
+              }).then(() => {
+                console.log(`Playing playlist: ${playlistId}`);
+              }).catch(error => {
+                console.error('Failed to play playlist:', error);
+              });
+            });
+          });
+        });
+      }
+    };
 
-  nextButton.addEventListener("click", function() {
-    // TODO: Handle next song logic
-    console.log("Playing next song...");
-  });
+    // Event listeners for emotion buttons
+    sadButton.addEventListener("click", () => {
+      playPlaylist('37i9dQZF1DX7qK8ma5wgG1');
+    });
 
-  shuffleButton.addEventListener("click", function() {
-    // TODO: Handle shuffle song logic
-    console.log("Shuffling songs...");
-  });
+    happyButton.addEventListener("click", () => {
+      playPlaylist('7s09coXLGbofhNrwSusr4G');
+    });
+
+    excitedButton.addEventListener("click", () => {
+      playPlaylist('3gIG7djN9Kn8l06ZDpn5iP');
+    });
+
+    indifferentButton.addEventListener("click", () => {
+      playPlaylist('5KvnL7siSa0tb6ZXFrqVih');
+    });
+  };
 });
